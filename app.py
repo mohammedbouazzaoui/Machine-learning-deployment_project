8299660c786893d505acf683bb6d16a5d4d438c6
Mohammed Bouazzaoui <98815410+mohammedbouazzaoui@users.noreply.github.com>
2022-05-05 11:04:53 +0200

    Auto script

--- app.py
+++ app.py
@@ -4,16 +4,6 @@ Created on Thu Apr 28 09:17:15 2022
 
 @author: bmadmin
 
-
-#### Step 4: Create your API
-
-In your `app.py` file, create a Flask API that contains:
-
-- A route at `/` that accept:
-  - `GET` request and return `"alive"` if the server is alive.
-- A route at `/predict` that accept:
-  - `POST` request that receives the data of a house in JSON format.
-  - `GET` request returning a string to explain what the `POST` expect (data and format).
 """
 import sys
 sys.path.append(".")
@@ -259,4 +249,4 @@ main
 debug(DEBUG,"after main")
 
 
-#app.run(host='localhost', port=5000)
+app.run(host='localhost', port=5000)
